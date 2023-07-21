af2920a4a8b6b6945574636c1da77f5805c90b09
shivanshc987 <shivanshc0987@outlook.com>
2023-07-21 16:40:43 +0530

    Added the route to handle the GET /numbers API

--- q1/main.py
+++ q1/main.py
@@ -25,3 +25,30 @@ def merge_and_sort(numbers_list):
     merged = list(set(merged)) # Remove any duplicates
     merged.sort() # Sort the merged list
     return merged
+
+# A route to handle the GET /numbers API
+@app.route("/numbers", methods=["GET"])
+def numbers():
+    urls = request.args.getlist("url") # Get the list of URLs from query parameters
+    queue = Queue() # Initialize a queue to store the numbers from each URL
+    threads = [] # Initialize a list to store the threads for each URL
+
+    for url in urls:
+        if url.startswith("http"): # Check if the URL is valid
+            thread = Thread(target=fetch_numbers, args=(url, queue)) # Create a thread to fetch numbers from the URL
+            thread.start() # Start the thread
+            threads.append(thread) # Append the thread to the list
+
+    for thread in threads:
+        thread.join() # Wait for all threads to finish
+
+    numbers_list = [] # Initialize an empty list to store the numbers from the queue
+    while not queue.empty():
+        numbers_list.append(queue.get()) # Get the numbers list from the queue and append it to the list
+
+    result = merge_and_sort(numbers_list) # Merge and sort the numbers list
+
+    return jsonify({"numbers": result}) # Return the JSON response with the result
+
+if __name__ == "__main__":
+    app.run(port=8008) # Run the app on port 8008
